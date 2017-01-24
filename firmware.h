822f0880a6f8974c386da041d112bdb62cfc1d9c
Lance Fetters <gaizin@gmail.com>
2017-01-24 16:45:39 +0900

    Updated firmware value for 9.3, added values for 10.0, 10.1, 10.2
    NOTE: These values were obtained via the respective simulators from
          Xcode 7.3, Xcode 8.0, Xcode 8.1, and Xcode 8.2.

--- firmware.h
+++ firmware.h
@@ -104,7 +104,19 @@
 #endif
 
 #ifndef kCFCoreFoundationVersionNumber_iOS_9_3
-#define kCFCoreFoundationVersionNumber_iOS_9_3 1280.30
+#define kCFCoreFoundationVersionNumber_iOS_9_3 1280.38
+#endif
+
+#ifndef kCFCoreFoundationVersionNumber_iOS_10_0
+#define kCFCoreFoundationVersionNumber_iOS_10_0 1348.00
+#endif
+
+#ifndef kCFCoreFoundationVersionNumber_iOS_10_1
+#define kCFCoreFoundationVersionNumber_iOS_10_1 1348.00
+#endif
+
+#ifndef kCFCoreFoundationVersionNumber_iOS_10_2
+#define kCFCoreFoundationVersionNumber_iOS_10_2 1348.22
 #endif
 
 // Comparison Macros
